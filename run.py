ec1ccfa5d7cd200766428fe28744ca2caf86c0a3
Vadim Makkar <vadimmakkar@gmail.com>
2021-10-16 13:02:47 +0300

    run.py to pip8

--- run.py
+++ run.py
@@ -11,7 +11,8 @@ from decimal import *
 
 def get_dict_currencieses(url):
     headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+        "user-agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/94.0.4606.71 Safari/537.36"),
         "accept": "text/css,*/*;q=0.1",
     }
     try:
@@ -23,18 +24,21 @@ def get_dict_currencieses(url):
     select_currencies = soup.find_all('select', id="UniDbQuery_VAL_NM_RQ")
     dict_currencies = {}
     for i in select_currencies[0].find_all("option"):
-        currencie = i.string.replace("\n", "").split("  ")[14].lower().replace("\r", "").rstrip()#HA-HA(!)
+        currencie = i.string.replace("\n", "").split("  ")[14].lower().replace("\r", "").rstrip()  # HA-HA(!)
         dict_currencies[currencie] = i.get("value")
     return dict_currencies
 
 
 def get_data(code_currencies):
     headers = {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36",
+        "user-agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/94.0.4606.71 Safari/537.36"),
         "accept": "text/css,*/*;q=0.1",
     }
     today = datetime.today().strftime('%d.%m.%Y')
-    url = f'https://cbr.ru/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.so=1&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ={code_currencies}&UniDbQuery.From=01.01.1992&UniDbQuery.To={today}'
+    url = (f'https://cbr.ru/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.so='
+           f'1&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ='
+           f'{code_currencies}&UniDbQuery.From=01.01.1992&UniDbQuery.To={today}')
     try:
         r = requests.get(url=url, headers=headers, timeout=5)
     except TooManyRedirects as e:
@@ -62,14 +66,15 @@ def create_table(name_table, columns):
             user=user,
             password=password,
             database=db_name,
-            port = port
+            port=port
         )
         connection.autocommit = True
 
-        #create a new table if not exist
+        # create a new table if not exist
         with connection.cursor() as cursor:
             cursor.execute(
-                f"CREATE TABLE IF NOT EXISTS {name_table}({columns[0]} DATE UNIQUE, {columns[1]} INTEGER, {columns[2]} REAL);")
+                f"CREATE TABLE IF NOT EXISTS {name_table}({columns[0]} DATE UNIQUE, "
+                f"{columns[1]} INTEGER, {columns[2]} REAL);")
             print(f"[INFO] Table {name_table} created successfully")
 
     except Exception as _ex:
@@ -80,9 +85,9 @@ def create_table(name_table, columns):
             # print("[INFO] PostgreSQL connection closed")
 
 
-def insert_data(name_table,columns, value):
+def insert_data(name_table, columns, value):
     try:
-    # connect to exist database
+        # connect to exist database
         connection = psycopg2.connect(
             host=host,
             user=user,
@@ -91,8 +96,9 @@ def insert_data(name_table,columns, value):
             port=port
         )
         connection.autocommit = True
-    #insert data into a table
-        insert = f"INSERT INTO {name_table} ({columns[0]}, {columns[1]}, {columns[2]}) VALUES ('{value[0]}',{int(value[1])}, {Decimal(value[2].replace(',', '.'))});"
+        # insert data into a table
+        insert = (f"INSERT INTO {name_table} ({columns[0]}, {columns[1]}, {columns[2]}) VALUES"
+                  f" ('{value[0]}',{int(value[1])}, {Decimal(value[2].replace(',', '.'))});")
         with connection.cursor() as cursor:
             cursor.execute(insert)
         # print("[INFO] Data was succefully inserted")
@@ -137,7 +143,6 @@ def main():
         print(f"В базу записано {count} значений валюты {currencies}")
     except Exception as _ex:
         print("На сайте нет такой валюты. Прощайте :(")
-
 
 
 if __name__ == "__main__":
